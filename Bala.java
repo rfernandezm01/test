555acabbb995ab7c0e40f8a017e1d4122bcb281a
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2021-05-17 18:29:03 +0200

    Update Bala.java

--- Bala.java
+++ Bala.java
@@ -2,24 +2,25 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-public class Bala {
 
-    Texture texture;
-    float x, y, w, h;
+public class Bala {
+    static Texture texture = new Texture("bala.png");
+
+    float x, y, w, h, v;
 
     Bala(float xNave, float yNave){
-        texture = new Texture("bala.png");
-        w = 30;
-        h = 60;
+        w = 20;
+        h = 50;
         x = xNave-w/2;
         y = yNave;
+        v = 12;
     }
-
 
     public void render(SpriteBatch batch) {
         batch.draw(texture, x, y, w, h);
     }
+
     void update(){
-        y += 12;
+        y += v;
     }
 }
